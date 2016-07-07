87108b4e459f80c40f373393b52029ff2078664e
Jo Seunghyun <sky@izz.kr>
2016-07-07 22:44:38 +0900

    Follow 모델 생성

---
+++ app/models/follow.rb
@@ -0,0 +1,3 @@
+class Follow < ActiveRecord::Base
+  belongs_to :group
+end
---
+++ db/migrate/20160707134248_create_follows.rb
@@ -0,0 +1,10 @@
+class CreateFollows < ActiveRecord::Migration
+  def change
+    create_table :follows do |t|
+      t.integer :user_id
+      t.integer :group_id
+
+      t.timestamps null: false
+    end
+  end
+end
