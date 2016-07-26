7d83858e6171c687fe72dfc982da6b0f9867f4e1
Jo Seunghyun <sky@izz.kr>
2016-07-27 00:53:14 +0900

    인증 메소드 추가

--- app/controllers/api/v1/api_controller.rb
+++ app/controllers/api/v1/api_controller.rb
@@ -1,11 +1,21 @@
 class Api::V1::ApiController < ApplicationController
   extend ParameterValidator
   skip_before_action :verify_authenticity_token
-  before_action :set_user
+  before_action :check_api_key
 
-  def set_user
-    #FIXME the code below is for development only
-    @user = User.find params[:current_user_id] rescue nil
+  def check_api_key
+    authenticate_or_request_with_http_token do |token, options|
+      api_key = ApiKey.where(access_token: token).first
+      if api_key.nil?
+        render_unauthorized and return
+      end
+      if api_key and api_key.revoked?
+        render status: 419, json: {
+          message: "The token is revoked at: #{api_key.revoked_at.strftime("%Y-%m-%d %H:%M:%S")}"
+        } and return
+      end
+      @user = api_key.user rescue nil
+    end
   end
 
   def render_unauthorized
