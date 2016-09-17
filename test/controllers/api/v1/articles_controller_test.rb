8e43a211a51e2e46675923bab15e55c15999cffd
Jo Seunghyun <sky@izz.kr>
2016-09-17 17:07:15 +0900

    ArticlesController 테스트 코드 작성

--- test/controllers/api/v1/articles_controller_test.rb
+++ test/controllers/api/v1/articles_controller_test.rb
@@ -1,7 +1,160 @@
 require 'test_helper'
 
 class Api::V1::ArticlesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "[articles#list] access token 없이 접근하면 실패" do
+    get :index
+    assert_response :unauthorized
+  end
+
+  test "[articles#list] access token이 잘못된 경우 실패" do
+    set_incorrect_access_token
+    get :index
+    assert_response :unauthorized
+  end
+
+  test "[articles#list] access token이 유효하지 않은 경우 실패" do
+    set_revoked_access_token
+    get :index
+    assert_response 419
+  end
+
+  test "[articles#list] 글 목록을 가져옴" do
+    set_access_token
+    10.times do
+      Profile.all.each do |profile|
+        Article.create(
+          writer: User.first,
+          profiles: [profile],
+          title: "title",
+          content: "content"
+        )
+      end
+    end
+    get :index, format: :json
+    assert_response :success
+  end
+
+  test "[articles#list] 특정 프로필의 글 목록을 가져옴" do
+    set_access_token
+    10.times do
+      Profile.all.each do |profile|
+        Article.create(
+          writer: User.first,
+          profiles: [profile],
+          title: "title",
+          content: "content"
+        )
+      end
+    end
+    profile_id = Profile.last.id
+    get :index, profile_id: profile_id, format: :json
+    assert_response :success
+    response = JSON.parse @response.body
+    assert response["articles"].all? {|x| x["profiles"].map{|y| y["id"]}.include? profile_id}
+  end
+
+  test "[articles#show] 특정 글을 조회" do
+    set_access_token
+    article = Article.last
+    get :show, id: article.id, format: :json
+    response = JSON.parse @response.body
+    assert_equal response["id"], article.id
+  end
+
+  test "[articles#create] 필수 parameter가 없는 경우 실패" do
+    set_access_token
+    profile_ids = [Profile.last.id].join(",")
+    post :create, title: "title", content: "content"
+    assert_response :bad_request
+    post :create, profile_ids: profile_ids, content: "content"
+    assert_response :bad_request
+    post :create, profile_ids: profile_ids, title: "title"
+    assert_response :bad_request
+  end
+
+  test "[articles#create] 글 생성" do
+    set_access_token
+    profile_ids = [Profile.last.id].join(",")
+    title = "titletitle"
+    content = "contentcontent"
+    post :create, profile_ids: profile_ids, title: title, content: content, format: :json
+    assert_response :success
+    response = JSON.parse @response.body
+    article = Article.find(response["id"])
+    assert_equal title, article.title
+    assert_equal content, article.content
+  end
+
+  test "[articles#create] 프로필 ID를 지정하지 않은 경우 실패" do
+    set_access_token
+    post :create, profile_ids: "", title: "title", content: "content", format: :json
+    assert_response :bad_request
+  end
+
+  test "[articles#update] 존재하지 않는 글을 수정하려고 하는 경우 실패" do
+    article_id = Article.last.id + 1
+    set_access_token
+    put :update, id: article_id, title: "title", content: "content"
+    assert_response :not_found
+  end
+
+  test "[articles#update] 자신이 작성하지 않은 글을 수정하려고 하는 경우 실패" do
+    article = Article.create(
+      writer: User.last,
+      profiles: [Profile.last],
+      title: "title",
+      content: "content"
+    )
+    set_access_token
+    put :update, id: article.id, title: "title", content: "content"
+    assert_response :unauthorized
+  end
+
+  test "[articles#update] 필수 parameter가 없는 경우 실패" do
+    set_access_token
+    article_id = Article.last.id
+    put :update, id: article_id, content: "content"
+    assert_response :bad_request
+    put :update, id: article_id, title: "title"
+    assert_response :bad_request
+  end
+
+  test "[articles#update] 글 수정" do
+    set_access_token
+    article_id = Article.last.id
+    title = "title2"
+    content = "content2"
+    put :update, id: article_id, title: title, content: content, format: :json
+    assert_response :success
+    article = Article.find(article_id)
+    assert_equal title, article.title
+    assert_equal content, article.content
+  end
+
+  test "[articles#destroy] 존재하지 않는 글을 삭제하려고 하는 경우 실패" do
+    article_id = Article.last.id + 1
+    set_access_token
+    delete :destroy, id: article_id
+    assert_response :not_found
+  end
+
+  test "[articles#destroy] 자신이 작성하지 않은 글을 삭제하려고 하는 경우 실패" do
+    article = Article.create(
+      writer: User.last,
+      profiles: [Profile.last],
+      title: "title",
+      content: "content"
+    )
+    set_access_token
+    delete :destroy, id: article.id
+    assert_response :unauthorized
+  end
+
+  test "[articles#destroy] 글 삭제" do
+    set_access_token
+    article_id = Article.last.id
+    delete :destroy, id: article_id
+    assert_response :success
+    assert Article.where(id: article_id).empty?
+  end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -7,4 +7,15 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+  def set_access_token(token="abcdef0123456789abcdef")
+    @request.headers["Authorization"] = "Token token=\"#{token}\""
+  end
+
+  def set_incorrect_access_token
+    set_access_token("0123456789abcdef")
+  end
+
+  def set_revoked_access_token
+    set_access_token("fedcba9876543210fedcba")
+  end
 end
