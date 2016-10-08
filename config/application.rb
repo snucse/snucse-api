063a4838d80e8ae64392d93b1a89876bc1331d23
Jo Seunghyun <sky@izz.kr>
2016-10-08 19:06:49 +0900

    API response를 camelCase로 수정

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module Snucse
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.generators.assets = false
+
+    Jbuilder.key_format camelize: :lower
   end
 end
