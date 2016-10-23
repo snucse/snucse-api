342a4235844a702a35082886d3d48195833f1b8b
Jo Seunghyun <sky@izz.kr>
2016-10-24 00:26:58 +0900

    태그 조회 API 문서 내용 추가

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -2,6 +2,12 @@ class Api::V1::TagsController < Api::V1::ApiController
   api! "태그의 정보를 전달한다."
   example <<-EOS
   {
+    "creator": {
+      "id": 1,
+      "username": "writer",
+      "name": "작성자",
+      "profileImageUri": "http://placehold.it/100x100"
+    },
     "articles": [
       {"id": 1, "title": "title", ...},
       ...
