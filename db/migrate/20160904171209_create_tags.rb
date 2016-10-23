014ac54d086ad6ff5df5a571e36a93bb1e66e3c8
Jo Seunghyun <sky@izz.kr>
2016-10-23 23:04:02 +0900

    태그 모델에 제한조건 및 인덱스 추가

--- db/migrate/20160904171209_create_tags.rb
+++ db/migrate/20160904171209_create_tags.rb
@@ -1,10 +1,11 @@
 class CreateTags < ActiveRecord::Migration
   def change
     create_table :tags do |t|
-      t.string :name
+      t.string :name, null: false
       t.integer :creator_id, null: false
 
       t.timestamps null: false
     end
+    add_index :tags, :name, unique: true
   end
 end
