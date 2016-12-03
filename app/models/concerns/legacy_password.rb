9c6f664e33a27a6922c4c5c262df76939d2f699e
Jo Seunghyun <sky@izz.kr>
2016-12-03 16:05:03 +0900

    legacy password version 1 마이그레이션 코드 추가

--- app/models/concerns/legacy_password.rb
+++ app/models/concerns/legacy_password.rb
@@ -3,19 +3,48 @@ module LegacyPassword
 
   def check_password(password)
     if self.legacy_password_digest
-      salt = self.legacy_password_digest[6..13].scan(/../).map{|x| x.to_i(16).chr}.join
-      hash = self.legacy_password_digest[14..-1].downcase
-      password2 = password.split(//).map{|x| x + "\x00"}.join
-      if Digest::SHA512.new.update(password2 + salt) == hash
-        self.update_attributes(
-          legacy_password_digest: nil,
-          password: password
-        )
-        return self
+      case self.legacy_password_digest.size
+      when 94
+        check_legacy_password_v1(password)
+      when 54
+        check_legacy_password_v1(password)
+      when 142
+        check_legacy_password_v2(password)
       else
-        return false
+        false
       end
+    else
+      self.authenticate(password)
     end
-    self.authenticate(password)
+  end
+
+  def check_legacy_password_v1(password)
+    salt = self.legacy_password_digest[6..13].scan(/../).map{|x| x.to_i(16).chr}.join
+    hash = self.legacy_password_digest[14..53].downcase
+    password2 = password.split(//).map{|x| x + "\x00"}.join
+    if Digest::SHA512.new.update(password2 + salt) == hash
+      self.update_attributes(
+        legacy_password_digest: nil,
+        password: password
+      )
+      self
+    else
+      false
+    end
+  end
+
+  def check_legacy_password_v2(password)
+    salt = self.legacy_password_digest[6..13].scan(/../).map{|x| x.to_i(16).chr}.join
+    hash = self.legacy_password_digest[14..-1].downcase
+    password2 = password.split(//).map{|x| x + "\x00"}.join
+    if Digest::SHA512.new.update(password2 + salt) == hash
+      self.update_attributes(
+        legacy_password_digest: nil,
+        password: password
+      )
+      self
+    else
+      false
+    end
   end
 end
