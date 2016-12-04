099c8925fa8a843ca4123349cbea9c9c520feba3
Jo Seunghyun <sky@izz.kr>
2016-12-04 17:34:48 +0900

    파일 배열 validator 추가

--- app/controllers/concerns/parameter_validator.rb
+++ app/controllers/concerns/parameter_validator.rb
@@ -42,4 +42,27 @@ module ParameterValidator
       "Must be #{@type}."
     end
   end
+  class FileArrayValidator < Apipie::Validator::BaseValidator
+    def initialize(param_description, argument)
+      super(param_description)
+      @type = argument
+    end
+
+    def validate(value)
+      return false unless value.is_a?(Array)
+      value.all? do |v|
+        v.is_a?(Rack::Test::UploadedFile) || v.is_a?(ActionDispatch::Http::UploadedFile)
+      end
+    end
+
+    def self.build(param_description, argument, options, block)
+      if argument == Array and options[:of] == File
+        self.new(param_description, argument)
+      end
+    end
+
+    def description
+      "Must be #{@type}."
+    end
+  end
 end
