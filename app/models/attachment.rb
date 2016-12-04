d59ddd3e191d6c2f9d9a505181073fca1f8fb836
Jo Seunghyun <sky@izz.kr>
2016-12-04 16:40:18 +0900

    파일 모델 수정

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,4 @@ class Attachment < ActiveRecord::Base
   belongs_to :uploader, class_name: User
   belongs_to :article
   mount_uploader :file, FileUploader
-
-  has_secure_password validations: false
-  validates :password_digest, presence: true, if: "uploader_id.nil? and legacy_password_digest.nil?"
 end
--- db/migrate/20161203081156_create_attachments.rb
+++ db/migrate/20161203081156_create_attachments.rb
@@ -1,11 +1,9 @@
 class CreateAttachments < ActiveRecord::Migration
   def change
     create_table :attachments do |t|
-      t.integer :article_id
+      t.integer :article_id, null: false
       t.integer :uploader_id
       t.string :file
-      t.string :password_digest
-      t.string :legacy_password_digest
 
       t.timestamps null: false
     end
