4e22f2ab4605c856a912ef623e555b2f0e368f2c
Jo Seunghyun <sky@izz.kr>
2016-12-12 00:00:58 +0900

    업로드 폴더를 공용 폴더로 지정

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :deploy_to, '/home/deploy/snucse'
 append :linked_files, 'config/database.yml', 'config/secrets.yml', 'config/redis.yml', 'config/elasticsearch.yml'
 
 # Default value for linked_dirs is []
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system', 'uploads'
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
