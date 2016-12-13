9a4c7ca877f067163b6f15ba28e65b4672827cbb
Jo Seunghyun <sky@izz.kr>
2016-12-13 15:41:48 +0900

    관리자 수정 API에서 ID를 username으로 받음

--- app/controllers/api/v1/profiles_controller.rb
+++ app/controllers/api/v1/profiles_controller.rb
@@ -109,15 +109,16 @@ class Api::V1::ProfilesController < Api::V1::ApiController
   end
 
   api! "프로필의 관리자를 다른 사용자로 바꾼다."
-  param :adminId, Integer, desc: "새로운 관리자의 ID", required: true
+  param :adminId, String, desc: "새로운 관리자의 ID", required: true
   error code: 401, desc: "자신이 관리자가 아닌 프로필을 수정하려고 하는 경우"
   def transfer
     @profile = Profile.find_by_sid! params[:id]
     if @user != @profile.admin
       render_unauthorized and return
     end
+    admin = User.find_by_username! params[:adminId]
     if @profile.update(
-      admin_id: params[:adminId]
+      admin_id: admin.id
     )
       render :show
     else
