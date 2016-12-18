5855c1c57585395125835da0092d4f8ab179c448
Jo Seunghyun <sky@izz.kr>
2016-12-18 20:46:33 +0900

    프로필 댓글 추천 API 응답 형식 변경

--- app/controllers/api/v1/profile_comments_controller.rb
+++ app/controllers/api/v1/profile_comments_controller.rb
@@ -124,6 +124,6 @@ class Api::V1::ProfileCommentsController < Api::V1::ApiController
     expire = DateTime.now.beginning_of_day.to_i + 60 * 60 * 24
     $redis.expireat(key, expire)
     @profile_comment.increment(:recommendation_count).save
-    render json: {}
+    render :show
   end
 end
