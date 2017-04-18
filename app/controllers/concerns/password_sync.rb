ef535b8cbb5cedc31874a96c994ebbc1d0a03141
Jo Seunghyun <sky@izz.kr>
2017-04-19 00:36:25 +0900

    회원정보 연동 코드 url encode 처리

--- app/controllers/concerns/password_sync.rb
+++ app/controllers/concerns/password_sync.rb
@@ -7,14 +7,14 @@ module PasswordSync
     year = birthday[0..3]
     month = birthday[5..6]
     date = birthday[8..9]
-    http.post("/Account/JoinSubmit.aspx", "terms=on&name=#{name}&birth_year=#{year}&birth_month=#{month}&birth_date=#{date}&account=#{username}&bs_number=#{bs_number}&ms_number=&phd_number=&graduate_year=&graduate_month=&email=#{email}&phone=#{phone_number}")
+    http.post("/Account/JoinSubmit.aspx", {terms: "on", name: name, birth_year: year, birth_month: month, birth_date: date, account: username, bs_number: bs_number, ms_number: nil, phd_number: nil, graduate_year: nil, graduate_month: nil, email: email, phone: phone_number}.to_query)
   end
 
   def check_password(username, password)
     if Rails.env.production?
       http = Net::HTTP.new("id.snucse.org", 443)
       http.use_ssl = true
-      resp = http.post("/Authentication/Login.aspx", "login_type=member_login&referrer=&redirect_mode=keep&secure=on&member_account=#{username}&member_password=#{password}")
+      resp = http.post("/Authentication/Login.aspx", {login_type: "member_login", referrer: nil, redirect_mode: "keep", secure: "on", member_account: username, member_password: password}.to_query)
       resp.code != "200"
     else
       password == username + username
@@ -24,13 +24,13 @@ module PasswordSync
   def sync_password(username, password, new_password)
     http = Net::HTTP.new("id.snucse.org", 443)
     http.use_ssl = true
-    resp = http.post("/Authentication/Login.aspx", "login_type=member_login&referrer=&redirect_mode=keep&secure=on&member_account=#{username}&member_password=#{password}")
+    resp = http.post("/Authentication/Login.aspx", {login_type: "member_login", referrer: nil, redirect_mode: "keep", secure: "on", member_account: username, member_password: password}.to_query)
     if resp.code == "200"
       return
     end
     resp2 = http.get("/User/ModifyPasswordForm.aspx?account=#{username}", {"Cookie" => resp["set-cookie"]})
     doc = Nokogiri::HTML(resp2.body)
     uid = doc.css("input[name=uid]").attr("value").value
-    http.post("/User/ModifyPasswordSubmit.aspx", "uid=#{uid}&current_password=#{password}&new_password=#{new_password}&new_password_confirm=#{new_password}", {"Cookie" => resp["set-cookie"]})
+    http.post("/User/ModifyPasswordSubmit.aspx", {uid: uid, current_password: password, new_password: new_password, new_password_confirm: new_password}.to_query, {"Cookie" => resp["set-cookie"]})
   end
 end
